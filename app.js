4d7e2e2059410432a42977558a7d7eba93bb94bf
Jonathan Nuno <jnuno.423@gmail.com>
2020-11-02 12:33:25 -0500

    added login and register

--- app.js
+++ app.js
@@ -5,38 +5,80 @@ const mustacheExpress = require('mustache-express')
 var bcrypt = require('bcryptjs')
 require('dotenv').config()
 const PORT = process.env.PORT_NUMBER
+const session = require('express-session')
+
+app.use(express.urlencoded())
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(session({
+    secret: process.env.SESSION_ID,
+    resave: false,
+    saveUninitialized: true
+}))
 
 app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.get('/register', (req,res) => {
+app.get('/register', (req, res) => {
     res.render('register')
 })
 
+app.get('/profile', (req, res) => {
+    res.render('profile')
+})
 
-app.post('/register', (req,res) => {
-    const username = req.body.username
+app.post('/register', (req, res) => {
+    const username = req.body.username.toLowerCase()
     const password = req.body.password
     const confirmPassword = req.body.confirmPassword
 
     if (password == confirmPassword) {
-
-    }else{
-        res.render('register', {message, 'Passwords do not match'})
+        bcrypt.genSalt(10, function (err, salt) {
+            bcrypt.hash(password, salt, function (err, hash) {
+                let user = models.User.build({
+                    username: username,
+                    password: hash,
+                })
+                user.save().then((savedUser) => {
+                    res.redirect('login')
+                })
+            })
+        })
+    } else {
+        res.render('register', { message: 'Passwords do not match'})
     }
-
 })
 
 app.post('/login', (req,res) => {
-
+    const username = req.body.username.toLowerCase()
+    const password = req.body.password
+    models.User.findAll({
+        where: {username: username}
+    }).then((users)=>{
+        const persistedUser = users.find(user => {
+            return user.username == username
+        })
+        if (persistedUser) {
+            bcrypt.compare(password, persistedUser.password, function (err, result) {
+                if (result) {
+                    req.session.username = persistedUser.dataValues.id
+                    console.log(req.session.username)
+                    res.redirect('/profile')
+                }
+                else{
+                    res.render('login',{message: 'Password do not match'})
+                }
+            })
+        }
+        else{
+            res.render('login',{message: 'Username does not exist'})
+        }
+    })
 })
 
-
-app.use(express.urlencoded())
-app.engine('mustache', mustacheExpress())
-app.set('views', './views')
-app.set('view engine','mustache')
 
 app.listen(PORT, () => {
     console.log('Server is running...')
